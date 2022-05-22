95903214adc9ec2d92350f74d5d625ab1bb04247
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-05-22 11:29:35 -0400

    M

--- dist/routes/v1/categories.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const category_1 = require("../../models/category");
-const factory_1 = require("../../controllers/factory");
-const router = (0, express_1.Router)();
-router.route('/').get((0, factory_1.fetchAll)(category_1.Category)).post((0, factory_1.createDoc)(category_1.Category));
-// router.route('/:id').patch(updateDoc(Model)).delete(deleteDoc(Model))
-exports.default = router;
