7b9972848553a207e0a7d039b2738e8f9b08eaee
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-05-26 11:41:43 -0400

    M

--- src/controllers/v1/fileUpload.ts
+++ src/controllers/v1/fileUpload.ts
@@ -3,28 +3,29 @@ import multer from 'multer'
 import { extname } from 'path'
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: function (req, file, cb) {
-    // const randomName = Math.random().toString(20)
-    // cb(null, `${Date.now()}-${file.originalname}${extname(file.originalname)}`)
-    cb(null, `${Date.now()}-${file.originalname}`)
-  },
+	destination: function (req, file, cb) {
+		cb(null, './public/uploads')
+	},
+	filename: function (req, file, cb) {
+		// const randomName = Math.random().toString(20)
+		// cb(null, `${Date.now()}-${file.originalname}${extname(file.originalname)}`)
+		cb(null, `${Date.now()}-${file.originalname}`)
+	},
 })
 const fileUpload = multer({
-  storage,
-  limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.includes('image')) {
-      cb(null, true)
-    } else {
-      cb(null, false)
-      const err = new Error('Only image format allowed!')
-      err.name = 'ExtensionError'
-      return cb(err)
-    }
-  },
+	storage,
+	limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
+	fileFilter: (req, file, cb) => {
+		console.log(file)
+		if (file.mimetype.includes('image')) {
+			cb(null, true)
+		} else {
+			cb(null, false)
+			const err = new Error('Only image format allowed!')
+			err.name = 'ExtensionError'
+			return cb(err)
+		}
+	},
 }).array('gallery', 10)
 
 // const fileUpload = (req: Request, res: Response, next: NextFunction) => {
